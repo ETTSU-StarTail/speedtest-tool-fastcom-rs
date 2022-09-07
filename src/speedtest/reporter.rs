66e2d70d17377316482deb76a8b76b9eec2a9a34
ETTSU-StarTail <46245666+ETTSU-StarTail@users.noreply.github.com>
2022-09-07 17:14:07 +0900

    copy 先が正しくなかったので修正

--- src/speedtest/reporter.rs
+++ src/speedtest/reporter.rs
@@ -11,7 +11,7 @@ pub fn get_result_csv(
     Ok(collections::HashMap::new())
 }
 
-// TODO: graph plotting.
+/// TODO: graph plotting.
 pub fn make_network_speed_graph(
     csv_file_path: &path::Path,
     dest_path: &path::Path,
@@ -40,18 +40,39 @@ pub fn upload_report(
         .join(result_file_name.clone() + ".html");
 
     if save_result_file_path.exists() {
-        fs::copy(save_result_file_path.clone(), upload_dir_path)?;
+        match fs::copy(
+            save_result_file_path.clone(),
+            upload_dir_path.join(result_file_name.clone() + ".csv"),
+        ) {
+            Ok(_) => log::info!("Success upload the result csv."),
+            Err(error) => {
+                log::error!("Failed upload the result csv.");
+                log::error!("{:?}", error);
+                panic!();
+            }
+        };
     } else {
-        log::warn!("result file not found.");
+        log::warn!(
+            "result file is not found: {}",
+            save_result_file_path.display()
+        );
     }
 
     if save_report_file_path.exists() || is_force {
-        make_network_speed_graph(
-            save_result_file_path.as_path(),
-            save_report_file_path.as_path(),
-        )?;
+        // TODO: graph plotting.
+        // make_network_speed_graph(
+        //     save_result_file_path.as_path(),
+        //     save_report_file_path.as_path(),
+        // )?;
 
-        fs::copy(save_report_file_path, upload_dir_path)?;
+        // match fs::copy(save_report_file_path, upload_dir_path.join(result_file_name.clone() + ".html")) {
+        //     Ok(_) => log::info!("Success upload the report html."),
+        //     Err(error) => {
+        //         log::error!("Failed upload the report html.");
+        //         log::error!("{:?}", error);
+        //         panic!();
+        //     }
+        // };
     } else {
         log::warn!("Not make report. Because exists report or is_force flag is false.")
     }
